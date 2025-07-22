dddd4373969813c3db169947e47227d31bec2b15
abyssvgc <abdeliniaramayo@gmail.com>
2025-07-22 14:17:05 -0400

    Minimal Account Contract and Deploy Script

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+1. Create a basic AA on Etheruem
+2. Create a basic AA on zkSync
+3. Deploy, and send a userOp / transaction through them
+    1. Not going to send an AA to Etheruem
+    2. But we will send a AA tx to zkSync
---
+++ script/SendPackedUserOp.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+
+contract DeployMinimalAccount is Script {
+    function run() public {}
+
+    function deployMinimalAccount() public {}
+}
